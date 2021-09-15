00119fe465435b6e2b3f574dae6ed9465b575a21
Derek <derek@dadian-smith.net>
2021-09-15 16:35:23 -0500

    Refactor to use TextField

--- src/components/Incrementer/Incrementer.js
+++ src/components/Incrementer/Incrementer.js
@@ -1,7 +1,7 @@
 import React, {useRef} from "react";
 import PropTypes from 'prop-types';
 import {v4 as uuidv4} from 'uuid';
-import {IconButton, InputAdornment, Input, InputLabel, FormControl, FormHelperText, makeStyles} from "@material-ui/core";
+import {IconButton, InputAdornment,TextField, makeStyles} from "@material-ui/core";
 import {AddCircleRounded as IncreaseIcon, RemoveCircleRounded as DecreaseIcon} from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
@@ -37,15 +37,7 @@ function simulateChange(input, onChange) {
 
 function Incrementer({
     label,
-    className: classNameProp = null,
-    DecreaseIconButtonProps = null,
-    error = false,
-    FormHelperTextProps = null,
     id = useRef(uuidv4()).current,
-    IncreaseIconButtonProps = null,
-    InputLabelProps = null,
-    InputProps = null,
-    helperText = null,
     max = null,
     min = 0,
     onChange = null,
@@ -56,8 +48,6 @@ function Incrementer({
 }) {
     const inputRef = useRef(null);
     const classes = useStyles();
-    const helperTextId = helperText && id ? `${id}-helper-text` : null;
-    const {inputProps: inputElementProps, ...InputComponentProps} = InputProps || {};
 
     const handleDecreaseClick = () => {
         inputRef.current?.stepDown();
@@ -112,102 +102,67 @@ function Incrementer({
     }
 
     return (
-        <FormControl
-            error={error}
-            className={[classes.root, classNameProp].join(" ")}
-            {...other}>
-            <InputLabel
-                className={classes.label}
-                disableAnimation
-                htmlFor={id}
-                {...InputLabelProps}>
-                {label}
-            </InputLabel>
-            <div className={classes.spinbuttonContainer}>
-                <Input
-                    aria-describedby={helperTextId}
-                    aria-valuemin={min}
-                    aria-valuemax={max}
-                    aria-valuenow={value}
-                    classes={{
-                        root: classes.input,
-                        input: classes.inputElement
-                    }}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label={`Increase ${label}`}
-                                className={[classes.incrementButton, classes.increaseButton].join(" ")}
-                                color="primary"
-                                disabled={max && value >= max}
-                                disableRipple
-                                edge="end"
-                                onClick={handleIncreaseClick}
-                                tabIndex={-1}
-                                {...IncreaseIconButtonProps}>
-                                <IncreaseIcon />
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    id={id}
-                    inputProps={{
-                        max,
-                        min,
-                        step,
-                        ...inputElementProps
-                    }}
-                    inputRef={inputRef}
-                    onChange={onChange}
-                    onKeyDown={handleKeyDown}
-                    startAdornment={
-                        <InputAdornment position="start">
-                            <IconButton
-                                aria-label={`Decrease ${label}`}
-                                className={[classes.incrementButton, classes.decreaseButton].join(" ")}
-                                color="primary"
-                                disabled={value <= min}
-                                disableRipple
-                                edge="start"
-                                onClick={handleDecreaseClick}
-                                tabIndex={-1}
-                                {...DecreaseIconButtonProps}>
-                                <DecreaseIcon />
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    type="number"
-                    value={value}
-                    {...InputComponentProps}
-                />
-            </div>
-            {helperText &&
-                <FormHelperText
-                    className={classes.helperText}
-                    id={helperTextId}
-                    {...FormHelperTextProps}>
-                    {helperText}
-                </FormHelperText>
-            }
-        </FormControl>
+        <TextField
+            aria-valuemin={min}
+            aria-valuemax={max}
+            aria-valuenow={value}
+            id={id}
+            label={label}
+            InputLabelProps={{disableAnimation: true}}
+            inputProps={{
+                max,
+                min,
+                step,
+            }}
+            InputProps={{
+                classes: {
+                    input: classes.inputElement
+                },
+                endAdornment:
+                    <InputAdornment position="end">
+                        <IconButton
+                            aria-label={`Increase ${label}`}
+                            className={[classes.incrementButton, classes.increaseButton].join(" ")}
+                            color="primary"
+                            disabled={max && value >= max}
+                            disableRipple
+                            edge="end"
+                            onClick={handleIncreaseClick}
+                            tabIndex={-1}>
+                            <IncreaseIcon />
+                        </IconButton>
+                    </InputAdornment>,
+                onKeyDown: handleKeyDown,
+                startAdornment:
+                    <InputAdornment position="start">
+                        <IconButton
+                            aria-label={`Decrease ${label}`}
+                            className={[classes.incrementButton, classes.decreaseButton].join(" ")}
+                            color="primary"
+                            disabled={value <= min}
+                            disableRipple
+                            edge="start"
+                            onClick={handleDecreaseClick}
+                            tabIndex={-1}>
+                            <DecreaseIcon />
+                        </IconButton>
+                    </InputAdornment>
+            }}
+            inputRef={inputRef}
+            type="number"
+            value={value}
+            variant="outlined"
+            {...other}
+        />
     )
 }
 
 Incrementer.propTypes = {
     label: PropTypes.string.isRequired,
-    className: PropTypes.string,
-    DecreaseIconButtonProps: PropTypes.object,
-    error: PropTypes.bool,
-    FormHelperTextProps: PropTypes.object,
     id: PropTypes.string,
-    IncreaseIconButtonProps: PropTypes.object,
-    InputLabelProps: PropTypes.object,
-    InputProps: PropTypes.object,
-    helperText: PropTypes.string,
     max: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     min: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     onChange: PropTypes.func,
-    onDecreaseClick: PropTypes.func,
-    onIncreaseClick: PropTypes.func,
     step: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     stepLarge: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     value: PropTypes.any
